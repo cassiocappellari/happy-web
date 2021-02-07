2c7f119e0221bb32097e8affdd3182fe2a6e13a0
Cassio Cappellari <cassiocappellari@gmail.com>
2021-02-07 15:55:01 -0300

    refactor: deletados todos arquivos desnecessarios do react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+interface TitleProps {
+  text: string
+}
+
+function Title(props: TitleProps) {
+  return <h1>{props.text}</h1>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title text="Título 1" />
+      <Title text="Título 2" />
+      <Title text="Título 3" />
+      <Title text="Título 4" />
     </div>
   );
 }
