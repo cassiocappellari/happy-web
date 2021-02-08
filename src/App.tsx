5bd3fe781e98300a27704cbbfb47da85434b9b48
Cassio Cappellari <cassiocappellari@gmail.com>
2021-02-08 08:08:56 -0300

    style: global css file created; css main styles added to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-
-interface TitleProps {
-  text: string
-}
-
-function Title(props: TitleProps) {
-  return <h1>{props.text}</h1>
-}
+import './styles/global.css'
 
 function App() {
   return (
     <div className="App">
-      <Title text="Título 1" />
-      <Title text="Título 2" />
-      <Title text="Título 3" />
-      <Title text="Título 4" />
+      <h1>Hello world!</h1>
     </div>
   );
 }
