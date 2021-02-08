6a8d6fc4557aba6c220836290a7c2c6ecb231b83
Cassio Cappellari <cassiocappellari@gmail.com>
2021-02-08 20:42:58 -0300

    feat: react-icons package added; style: FiArrowRight icon added in the landing page; landing.svg added as a background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FiArrowRight} from 'react-icons/fi'
 
 import './styles/global.css'
 import './styles/pages/landing.css'
@@ -21,7 +22,7 @@ function App() {
         </div>
 
         <a href="" className="enter-app">
-
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
         </a>
       </div>
     </div>
