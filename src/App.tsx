f19312a39efb49ace636f4ed70fd1137b96e95a1
Cassio Cappellari <cassiocappellari@gmail.com>
2021-02-08 08:47:52 -0300

    style: landing page css file created; landing page updated with css style, headers, logo and location

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+
 import './styles/global.css'
+import './styles/pages/landing.css'
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Porto Alegre</strong>
+          <span>Rio Grande do Sul</span>
+        </div>
+
+        <a href="" className="enter-app">
+          
+        </a>
+      </div>
     </div>
   );
 }
