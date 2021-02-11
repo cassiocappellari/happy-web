4eafe11cfceb4e92d25c96c1bef9af6e37ed559e
Cassio Cappellari <cassiocappellari@gmail.com>
2021-02-11 20:21:06 -0300

    style: orphanages-map css file created and updated; OrphanagesMap page updated with css style

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {FiPlus} from 'react-icons/fi'
+
+import mapMarkerImg from '../images/map-marker.svg'
+import '../styles/pages/orphanages-map.css'
 
 function OrphanagesMap() {
     return (
-        <h1>Hello world!</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Porto Alegre</strong>
+                    <span>Rio Grande do Sul</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     )
 }
 
